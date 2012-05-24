e0079838ea090a21fb714ccfb49e719dfb26fbe1
Mihail Klopotnyuk <m.klopotnuk@gmail.com>
2012-05-25 01:47:04 +0600

    terminal sakura and editor emacs

--- rc.lua
+++ rc.lua
@@ -38,8 +38,8 @@ end
 beautiful.init("/usr/share/awesome/themes/default/theme.lua")
 
 -- This is used later as the default terminal and editor to run.
-terminal = "xterm"
-editor = os.getenv("EDITOR") or "nano"
+terminal = "sakura"
+editor = os.getenv("EDITOR") or "emacs"
 editor_cmd = terminal .. " -e " .. editor
 
 -- Default modkey.
