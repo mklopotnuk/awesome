415efc9d55cf73e34a5168f1c360e49af95df14a
Mihail Klopotnyuk <m.klopotnuk@gmail.com>
2012-05-31 22:14:14 +0600

    Shell urxvt

--- rc.lua
+++ rc.lua
@@ -45,7 +45,7 @@ end
 beautiful.init("/usr/share/awesome/themes/default/theme.lua")
 
 -- This is used later as the default terminal and editor to run.
-terminal = "urxvtc"
+terminal = "urxvt"
 editor = os.getenv("EDITOR") or "emacs"
 editor_cmd = terminal .. " -e " .. editor
 distr = os.getenv("OS")
